311aac8e8954b082d92cf3eacc508ef1dcdd9deb
Janyny Barbosa <128196138+janynyb1@users.noreply.github.com>
2023-06-28 17:30:59 -0300

    Add files via upload
    utilizando postman , mysql workbench, net6.0 , visual studio 2022 .

---
+++ FilmeApi/Controllers/FilmeController.cs
@@ -0,0 +1,46 @@
+﻿using FilmeApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmeApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    // ...
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new
+            List<Filme>();
+        private static int id = 0;
+
+        [HttpPost]
+        public void AdicionaFilme([FromBody] Filme filme)
+        {
+            filme.Id = id++;
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+            Console.WriteLine(filme.Duracao);
+
+        }
+        [HttpGet]
+
+        public IEnumerable<Filme> RecuperaFilme([FromQuery] int skip, [FromQuery] int take)
+        {
+            return filmes.Skip(skip).Take(take);
+        }
+        [HttpGet("{id}")]
+
+        public IActionResult RecuperaFilmePorId(int id)
+        {
+            var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+
+            if (filme == null) return NotFound();
+
+            return Ok(filme);
+        }
+
+    }
+}
+
+
+             
+        
---
+++ FilmeApi/Data/FilmeContext.cs
@@ -0,0 +1,18 @@
+﻿using FilmeApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmeApi.Data
+
+{
+    public class FilmeContext : DbContext
+
+    {
+        public FilmeContext(DbContextOptions<FilmeContext> opts)
+            :base(opts)
+        {
+
+        }
+        public DbSet<Filme>Filmes { get; set; }
+
+    }
+}
